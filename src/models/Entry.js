6ae76cc9e6463f95da878a02fb32fc9434836cfe
gregkok <grkokkoris@gmail.com>
2025-03-05 21:42:46 +0100

    add more tags

--- src/models/Entry.js
+++ src/models/Entry.js
@@ -45,7 +45,7 @@ const entrySchema = new mongoose.Schema({
   },
   tags: {
     type: [String],
-    enum: ['HOUSING', 'UTILITIES', 'TRANSPORTATION', 'FOOD', 'DEBT', 'INSURANCE', 'SUBSCRIPTION', 'ENTERTAINMENT', 'MEDICAL', 'CAR', 'MISC'],
+    enum: ['HOUSING', 'UTILITIES', 'TRANSPORTATION', 'FOOD', 'DEBT', 'INSURANCE', 'SUBSCRIPTION', 'ENTERTAINMENT', 'MEDICAL', 'CAR', 'MISC', 'TRAVEL', 'GIFT', 'INVESTMENT', 'OTHER'],
     required: false,
   },
 }, { timestamps: true });
